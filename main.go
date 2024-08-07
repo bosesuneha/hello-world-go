db20b4a94d30d3025e6b3c67deffe9f6aa39f3e8
Suneha Bose <sunehabose@microsoft.com>
2024-08-07 12:23:18 -0700

    added endpoint for liveness/readiness probe

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
+	"fmt"
+	"net/http"
+	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("OK"))
+}
+
 func main() {
-	logrus.Println("Hello, World!")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/", rootHandler)
+
+	addr := fmt.Sprintf(":%s", port)
+	fmt.Printf("Hello, world! Starting server on %s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+	}
 }
