92e11c8da6fabf13589bc38486e591fa0ecc7bf3
Suneha Bose <sunehabose@microsoft.com>
2024-08-07 12:34:48 -0700

    updated port

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "80"
 	}
 
 	http.HandleFunc("/", rootHandler)
